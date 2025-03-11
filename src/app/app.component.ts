f8244eecf6f97eb1f439073e901d4163c189dbed
mohamed emad elhnawey <mohamedelhnawey676@gmail.com>
2025-03-11 17:45:09 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroComponent,AboutComponent,FooterComponent,PortfolioComponent,SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
